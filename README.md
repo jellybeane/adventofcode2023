f592a78816a21e8e4a5873057b5819ee621d119e
jellybeane <elly.shao.94@gmail.com>
2023-11-30 21:57:12 -0800

    day1

---
+++ README.md
@@ -0,0 +1,7 @@
+# adventofcode 2023
+Advent Of Code 2023
+Another year of Rust
+
+Day | Theme | Notes
+----|-------|-------
+1   |TODO| TODO
--- src/day1.rs
+++ src/day1.rs
@@ -2,54 +2,165 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 use anyhow::Result;
 
-type Data = usize;
+type Data = String;
 
-#[aoc_generator(dayX)]
+// each line originally contained a specific
+// calibration value that the Elves now need to recover.
+// On each line, the calibration value can be found by combining the 
+// first digit and the last digit (in that order) to form 
+// a single two-digit number.
+#[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Result<Vec<Data>> {
     input_generator_inner(input)
 }
 fn input_generator_inner(input: &str) -> Result<Vec<Data>> {
-    unimplemented!()
+    let mut result: Vec<String> = vec![];
+    for line in input.lines() {
+        result.push(line.to_string());
+    }
+
+    Ok(result)
 }
 
-#[aoc(dayX, part1)]
-pub fn solve_part1(input: &[Data]) -> usize {
+// What is the sum of all the calibration values?
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &[Data]) -> u32 {
     solve_part1_inner(input)
 }
-fn solve_part1_inner(input: &[Data]) -> usize {
-    unimplemented!()
+fn solve_part1_inner(input: &[Data]) -> u32 {
+    let mut result: Vec<u32> = vec![];
+    for line in input {
+        let mut tens: u32 = 0;
+        let mut ones: u32 = 0;
+        for c in line.chars() {
+            if c.is_digit(10) {
+                if 0 == tens {
+                    tens = c.to_digit(10).unwrap() * 10;
+                }
+                ones = c.to_digit(10).unwrap();
+            }
+        }
+        result.push(tens + ones);
+    }
+
+    result.iter().sum()
 }
 
-#[aoc(dayX, part2)]
-pub fn solve_part2(input: &[Data]) -> usize {
+// Some digits are spelled out with letters
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &[Data]) -> u32 {
     solve_part2_inner(input)
 }
-fn solve_part2_inner(input: &[Data]) -> usize {
-    unimplemented!()
+fn solve_part2_inner(input: &[Data]) -> u32 {
+    let mut result: Vec<u32> = vec![];
+    for line in input {
+        let mut tens: u32 = 0;
+        let mut ones: u32 = 0;
+        let mut maybespelled = "".to_owned();
+        for c in line.chars() {
+            if c.is_digit(10) {
+                if 0 == tens {
+                    tens = c.to_digit(10).unwrap() * 10;
+                }
+                ones = c.to_digit(10).unwrap();
+            }
+            else {
+                {
+                    maybespelled = format!("{}{}", maybespelled, c);
+                    let maybenum = spelledout(&maybespelled);
+                    if maybenum != 0 {
+                        if 0 == tens {
+                            tens = maybenum * 10;
+                        }
+                        ones = maybenum;
+                        // last/first letter might be shared, eg "eightwo"
+                        maybespelled = maybespelled.chars().last().unwrap().to_string();
+                    }
+                    
+                }
+            }
+        }
+        result.push(tens + ones);
+    }
+
+    result.iter().sum()
+}
+
+
+fn spelledout(maybenum: &str) -> u32 {
+    // ??? there's gotta be a better way
+    if maybenum.contains("one")
+    {
+       return 1
+    }
+    else if maybenum.contains("two")
+    {
+        return 2
+    }
+    else if maybenum.contains("three")
+    {
+        return 3
+    }
+    else if maybenum.contains("four")
+    {
+        return 4
+    }
+    else if maybenum.contains("five")
+    {
+        return 5
+    }
+    else if maybenum.contains("six")
+    {
+        return 6
+    }
+    else if maybenum.contains("seven")
+    {
+        return 7
+    }
+    else if maybenum.contains("eight")
+    {
+        return 8
+    }
+    else if maybenum.contains("nine")
+    {
+        return 9
+    }
+
+    0
 }
 
 #[cfg(test)]
 mod test {
-    const TEST_INPUT: &'static str =
-r#"REPLACE
-ME
-WITH
-TEST
-INPUT
+    const TEST_INPUT1: &'static str =
+r#"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+"#;
+const TEST_INPUT2: &'static str =
+r#"two1nine
+eightwothree
+eightwo
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+treb7uchet
 "#;
     #[test]
     fn test_part1_example() {
-        let input = super::input_generator(TEST_INPUT).unwrap();
+        let input = super::input_generator(TEST_INPUT1).unwrap();
         let result = super::solve_part1(&input);
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 142);
     }
 
     #[test]
     fn test_part2_example() {
-        let input = super::input_generator(TEST_INPUT).unwrap();
+        let input = super::input_generator(TEST_INPUT2).unwrap();
         let result = super::solve_part2(&input);
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 281 + 82 + 77);
     }
 }
