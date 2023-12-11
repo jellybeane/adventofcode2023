77ad82f43e410056cf129dbc1b5eaea767e6dd7b
jellybeane <elly.shao.94@gmail.com>
2023-12-10 22:44:07 -0800

    day11 cleanup

--- src/day11.rs
+++ src/day11.rs
@@ -17,88 +17,18 @@ fn input_generator_inner(input: &str) -> Result<Data> {
         for c in line.chars() {
             row.push(c);
         }
-
         rows.push(row);
     }
 
     Ok(rows)
 }
 
-fn expand(original: &Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let (empty_rows, empty_columns) = find_empty(original);
-    let original_numcols = original[0].len();
-    let numcols = original_numcols + empty_columns.len();
-
-    let mut expandedrows = vec![];
-    for (i, row) in original.iter().enumerate() {
-        if empty_rows.contains(&i) {
-            let emptyrow = vec!['.'; numcols];
-            expandedrows.push(emptyrow);
-            let emptyrow = vec!['.'; numcols];
-            expandedrows.push(emptyrow);
-
-        }
-        else {
-            let mut expandedrow = vec![];
-            for (j, &c) in row.iter().enumerate() {
-                if empty_columns.contains(&j) {
-                    assert_eq!('.', c);
-                    expandedrow.push('.');
-                    expandedrow.push('.');
-                }
-                else {
-                    expandedrow.push(c);
-                }
-            }
-            expandedrows.push(expandedrow);
-        }
-    }
-
-    expandedrows
-
-}   
-
 #[aoc(day11, part1)]
 pub fn solve_part1(input: &Data) -> usize {
     solve_part1_inner(input)
 }
 fn solve_part1_inner(input: &Data) -> usize {
-    let expanded = expand(input);
-    //dbg!(&expanded);
-
-    let mut galaxies = vec![];
-    for (i, row) in expanded.iter().enumerate() {
-        for (j, &c) in row.iter().enumerate() {
-            if '#' == c {
-                galaxies.push((i, j));
-            }
-        }
-    }
-
-    // distance from every galaxy to every other galaxy
-    let mut sum = 0;
-    for g1 in 0..(galaxies.len()-1) {
-        for g2 in (g1+1)..galaxies.len() {
-            let galaxy1 = galaxies[g1];
-            let galaxy2 = galaxies[g2];
-            let distance = manhattan(galaxy1, galaxy2);
-            //dbg!(g1, g2, distance);
-
-            sum += distance;
-        }
-    }
-
-    sum
-}
-
-fn manhattan(a: (usize, usize), b:(usize, usize)) -> usize
-{
-    let xmin = min(a.0, b.0);
-    let xmax = max(a.0, b.0);
-    let ymin = min(a.1, b.1);
-    let ymax = max(a.1, b.1);
-
-    (xmax - xmin) + (ymax - ymin)
+    solve_everything(input, 2)
 }
 
 fn find_empty(original: &Vec<Vec<char>>) -> (Vec<usize>, Vec<usize>) {
@@ -108,7 +38,6 @@ fn find_empty(original: &Vec<Vec<char>>) -> (Vec<usize>, Vec<usize>) {
         if row.iter().all(|&c| '.' == c) {
             empty_rows.push(i);
         }
-        
     }
 
     let original_numcols = original[0].len();
@@ -129,10 +58,10 @@ fn find_empty(original: &Vec<Vec<char>>) -> (Vec<usize>, Vec<usize>) {
 }
 
 fn expanded_manhattan(a: (usize, usize), b:(usize, usize),
-    empty_rows: &[usize], empty_cols: &[usize]) -> usize
+    empty_rows: &[usize], empty_cols: &[usize], expansion: usize) -> usize
 {
-    let expansion = 1000000 - 1;
-    //let expansion = 10 - 1;
+    // don't double-count the already-existing empty row/column
+    let expansion = expansion - 1;
 
     let rowmin = min(a.0, b.0);
     let rowmax = max(a.0, b.0);
@@ -161,8 +90,12 @@ pub fn solve_part2(input: &Data) -> usize {
     solve_part2_inner(input)
 }
 fn solve_part2_inner(input: &Data) -> usize {
-    let (empty_rows, empty_columns) = find_empty(input);
+    solve_everything(input, 1000000)
+}
 
+fn solve_everything(input: &Data, expansion: usize) -> usize {
+    let (empty_rows, empty_columns) = find_empty(input);
+    
     let mut galaxies = vec![];
     for (i, row) in input.iter().enumerate() {
         for (j, &c) in row.iter().enumerate() {
@@ -178,8 +111,7 @@ fn solve_part2_inner(input: &Data) -> usize {
         for g2 in (g1+1)..galaxies.len() {
             let galaxy1 = galaxies[g1];
             let galaxy2 = galaxies[g2];
-            let distance = expanded_manhattan(galaxy1, galaxy2, &empty_rows, &empty_columns);
-            //dbg!(g1, g2, distance);
+            let distance = expanded_manhattan(galaxy1, galaxy2, &empty_rows, &empty_columns, expansion);
 
             sum += distance;
         }
@@ -213,7 +145,7 @@ r#"...#......
     #[test]
     fn test_part2_example() {
         let input = super::input_generator(TEST_INPUT).unwrap();
-        let result = super::solve_part2(&input);
+        let result = super::solve_everything(&input, 10);
 
         assert_eq!(result, 1030);
     }
